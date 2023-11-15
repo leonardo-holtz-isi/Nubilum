110bab896b42a3cc8b563753a5977c36ca16103c
Leonardo Holtz <leo.holtz98@gmail.com>
2023-11-15 18:31:34 -0300

    update setup.py to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Nubilum',
-    version='0.1',
+    version='0.1.1',
     description='An explainability library for instance segmentation in \
         point clouds',
     author='Leonardo Holtz de Oliveira',
